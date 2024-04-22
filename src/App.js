d6a0fddc98f014b1b0f6f8128bda5725e05f75c2
yeseong33 <ohyeesung@naver.com>
2024-04-22 15:52:36 +0900

    feat: make component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+/* eslint-disable */
+
 import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js'
+import { MyNav } from './MyNav.js';
 
 function App() {
+
+  let [shoes] = useState(data)
+
+  console.log(shoes[0])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyNav/>
+      {/* <div className='main-bg' style={{backgroundImage : 'url('+ bg +')'}}></div> */}
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          {
+            shoes.map((s, idx) => {
+              return (
+                <Shoes shoes={s} idx={idx} key={idx}/>
+              )
+            })
+          }
+        </Row>
+      </Container>
     </div>
   );
 }
+
+
+function Shoes (props) {
+  return (
+    <>
+      <Col xs={{ order: 'last' }}>
+        <img src={process.env.PUBLIC_URL + '/shoes'+props.idx+'.jpg'} width="80%" onClick={() => {
+          PopStateEvent
+        }}/>
+        <h4>{props.shoes.title}</h4>
+        <p> {props.shoes.content} </p>
+        <p> {props.shoes.price} </p>
+      </Col>
+    </>
+  )
+}
+
 
 export default App;
