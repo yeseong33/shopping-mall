2b79a53706ddc0d0c8415323464754dd121b61f9
yeseong33 <ohyeesung@naver.com>
2024-04-24 17:28:36 +0900

    feat: alert input

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,22 +1,74 @@
 /* eslint-disable */
 
-import { Outlet, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { Outlet, useParams, useSearchParams } from "react-router-dom";
+import styled from "styled-components";
 
+
+const AlertInput = styled.input`
+    width : 300px;
+    height : 40px;
+    fontSize : 16px;
+    padding : 5px;
+` 
+
+// const ColorBtn = styled.button`
+//     background : ${ props => props.bg };
+//     color : ${ props => props.bg == 'blue' ? 'white' : 'black'};
+//     padding; 10px;
+// `
+
+// const NewBtn = styled.button(ColorBtn)`
+
+// `
 
 function Detail(props) {
+
+
+    let [count, setCount] = useState(0)
+    let [alert, setAlert] = useState(true)
     let {id} = useParams();
+    let [userInput, setUserInput] = useState('');
+    let [intAlert, setIntAlert] = useState(false);
     let item = props.shoes.find((x) => 
         x.id == id
     )
 
+    useEffect(() => {
+        setTimeout(() => {setIntAlert(false)}, 5000)
+
+        // useEffect 사용 전에 실행
+        return () => {
+            // 기존 타이머는 제거하는 코드
+            // clearTimeout(a)
+        }
+    }) 
+
+
     return (
         <div className="container">
+            {count}
+            <button onClick={()=> setCount(count+1)}>버튼</button>
             <div className="row">
                 {item == null ? <p>상품이 존재하지 않습니다.</p> : 
                 <>
                     <div className="col-md-6">
                         <img src={process.env.PUBLIC_URL + '/shoes' + id + '.jpg'} width="100%" />
                     </div>
+                    {
+                        intAlert == true ? 
+                        <div className="alert alert-warning">
+                        경고 : 숫자 입력!
+                        </div> 
+                        : null
+                    }
+                    <AlertInput type="text" placeholder="여기에 입력하세요" value={userInput} onInput={(e) => {
+                            isNaN(e.target.value) ? 
+                            setIntAlert(true) : 
+                            setUserInput(e.target.value)
+                        }}>
+                    </AlertInput>
                     <div className="col-md-6">
                         <p>{item.content}</p>
                         <p>{item.price}</p>
@@ -34,26 +86,3 @@ function Detail(props) {
 
 
 export { Detail };
-
-// /* eslint-disable */
-
-
-// function Detail(props) {
-//     return (
-//         <div className="container">
-//             <div className="row">
-//                 <div className="col-md-6">
-//                 <img src={process.env.PUBLIC_URL + '/shoes'+props.idx+'.jpg'} width="100%" />
-//                 </div>
-//                 <div className="col-md-6">
-//                 <h4 className="pt-5">{props.shoes.title}</h4>
-//                 <p>{props.shoes.content}</p>
-//                 <p>{props.shoes.price}</p>
-//                 <button className="btn btn-danger">주문하기</button> 
-//                 </div>
-//             </div>
-//         </div> 
-//     )
-// }
-
-// export { Detail };
