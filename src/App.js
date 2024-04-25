894ea0846e6f7d4545c03c90e77c7ebd98c425ce
yeseong33 <ohyeesung@naver.com>
2024-04-25 11:20:28 +0900

    feat: context api

--- src/App.js
+++ src/App.js
@@ -2,52 +2,127 @@
 
 import './App.css';
 import { Col, Container, Row } from 'react-bootstrap';
-import { useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import data from './data.js'
 import { MyNav } from './MyNav.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import { Detail } from './routes/Detail.js';
 import { DetailInfo } from './routes/Detail-info.js';
+import axios from 'axios';
+import styled, { keyframes } from 'styled-components';
+
+// 스피너 애니메이션 정의
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+// 로딩 컨테이너 스타일
+const LoadingContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.9);
+  z-index: 1000;
+`;
+
+// 스피너 스타일
+const LoadingSpinner = styled.div`
+  border: 5px solid #f3f3f3;
+  border-top: 5px solid #3498db;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  animation: ${spin} 2s linear infinite;
+`;
+
+// 로딩 텍스트 스타일
+const LoadingText = styled.p`
+  margin-top: 10px;
+  color: #333;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+// state 보관함
+export let Context1 = createContext();
+
+
 
 function App() {
 
   let [shoes, setShoes] = useState(data)
+  let [page, setPage] = useState(2)
+  let [isLoading, setIsLoading] = useState(false);
   let navigate = useNavigate();
-  shuffleArray(shoes)
+  let [storage, setStorage] = useState([10, 12, 13]);
+  // let [shoes2, setShoes2] = useStatee('')
+
+  useEffect(() => {
+    shuffleArray(shoes)
+    return () => {
+    }
+  })
 
   return (
     <div className="App">
-      <MyNav/>
-      {/* <div className='main-bg' style={{backgroundImage : 'url('+ bg +')'}}></div> */}
+      <MyNav />
+      <div>
+            {isLoading ? (
+                <LoadingContainer>
+                    <LoadingSpinner />
+                    <LoadingText>로딩 중...</LoadingText>
+                </LoadingContainer>
+            ) : null}
+        </div>
 
       <Routes>
         {/* 메인 페이지 */}
         <Route path='/' element={
           <>
-          <div className="main-bg"></div>
-          <Container>
-            <Row>
-              {
-                shoes.map((s, idx) => {
-                  return (
-                    <Shoes shoes={s} idx={s.id} key={idx}/>
-                  )
-                })
-              }
-            </Row>
-          </Container>
+            <div className="main-bg"></div>
+            <Container>
+              <Row>
+                {
+                  shoes.map((s, idx) => {
+                    return (
+                      <Shoes shoes={s} idx={s.id} key={idx} />
+                    )
+                  })
+                }
+              </Row>
+              <button onClick={() => {
+                setIsLoading(true)
+                axios.get('https://codingapple1.github.io/shop/data' + page + '.json')
+                  .then((result) => {
+                    let copy = [...shoes, ...result.data]
+                    setShoes(copy)
+                    setPage(page+1)
+                    setTimeout(() => {setIsLoading(false)}, 1000)
+                  })
+                  .catch(() => {
+                    setTimeout(() => {setIsLoading(false)}, 1000)
+                  })
+              }}>더보기</button>
+            </Container>
           </>
-        }/>
+        } />
 
         {/* 404 페이지 */}
-        <Route path='*' element={<div> 없서용 </div>}/>
-        
+        <Route path='*' element={<div> 없서용 </div>} />
+
         {/* 신발 구매 페이지 */}
         <Route path='detail/'>
-          <Route path=':id' element={<Detail shoes={shoes}/>}></Route>
+          <Route path=':id' element={<Context1.Provider value= {{storage, shoes}}><Detail shoes={shoes}/></Context1.Provider>}></Route>
         </Route>
-
       </Routes>
+
+
     </div>
   );
 }
@@ -65,15 +140,19 @@ function About() {
 }
 
 
-function Shoes (props) {
+function Shoes(props) {
   let navigate = useNavigate();
 
   return (
     <>
-      <Col xs={{ order: 'last' }} onClick={() => {navigate('/detail/'+props.idx)}}>
+      <Col xs={{ order: 'last' }} onClick={() => { navigate('/detail/' + props.idx) }}>
 
-        <img src={process.env.PUBLIC_URL + '/shoes'+props.idx+'.jpg'} width="80%" onClick={() => {
-        }}/>
+        {/* 로컬 */}
+        {/* <img src={process.env.PUBLIC_URL + '/shoes'+props.idx+'.jpg'} width="80%" onClick={() => {
+        }}/> */}
+        {/* 외부 */}
+        <img src={'https://codingapple1.github.io/shop/shoes' + (props.idx + 1) + '.jpg'} width="80%" onClick={() => {
+        }} />
         <h4>{props.shoes.title}</h4>
         <p> {props.shoes.content} </p>
         <p> {props.shoes.price} </p>
@@ -84,8 +163,8 @@ function Shoes (props) {
 
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
 
   return array;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,20 @@
 /* eslint-disable */
 
-import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { useContext, useEffect, useState } from "react";
+import { Button, Col, Nav, Row, Tab, Tabs } from "react-bootstrap";
 import { Outlet, useParams, useSearchParams } from "react-router-dom";
 import styled from "styled-components";
 
+import './Detail.css';
+
+import {Context1} from './../App.js' 
 
 const AlertInput = styled.input`
     width : 300px;
     height : 40px;
     fontSize : 16px;
     padding : 5px;
-` 
+`
 
 // const ColorBtn = styled.button`
 //     background : ${ props => props.bg };
@@ -25,64 +28,128 @@ const AlertInput = styled.input`
 
 function Detail(props) {
 
+    let {storage} = useContext(Context1)
+
 
     let [count, setCount] = useState(0)
     let [alert, setAlert] = useState(true)
-    let {id} = useParams();
+    let { id } = useParams();
     let [userInput, setUserInput] = useState('');
     let [intAlert, setIntAlert] = useState(false);
-    let item = props.shoes.find((x) => 
+    let [tab, setTab] = useState(0);
+    let [img, setImg] = useState('');
+    let item = props.shoes.find((x) =>
         x.id == id
     )
-
+    console.log(img)
     useEffect(() => {
-        setTimeout(() => {setIntAlert(false)}, 5000)
-
+        setTimeout(() => { setIntAlert(false) }, 5000)
+        setTimeout(() => {setImg('initiationEnd')}, 100)
+        
         // useEffect 사용 전에 실행
         return () => {
             // 기존 타이머는 제거하는 코드
             // clearTimeout(a)
+            setImg('')
         }
-    }) 
+    }, [])
 
 
     return (
         <div className="container">
-            {count}
-            <button onClick={()=> setCount(count+1)}>버튼</button>
-            <div className="row">
-                {item == null ? <p>상품이 존재하지 않습니다.</p> : 
-                <>
-                    <div className="col-md-6">
-                        <img src={process.env.PUBLIC_URL + '/shoes' + id + '.jpg'} width="100%" />
-                    </div>
-                    {
+            {/* <button onClick={()=> setCount(count+1)}>버튼</button> */}
+            <div className="row align-items-center">
+                {item == null ? <p>상품이 존재하지 않습니다.</p> :
+                    <>
+                        <div className="col-md-6">
+                            {/* <img src={process.env.PUBLIC_URL + '/shoes' + id + '.jpg'} width="100%" /> */}
+                            <img className={"initiationStart "+img} src={'https://codingapple1.github.io/shop/shoes' + (item.id + 1) + '.jpg'} width="100%"></img>
+                        </div>
+                        {/* {
                         intAlert == true ? 
                         <div className="alert alert-warning">
                         경고 : 숫자 입력!
                         </div> 
                         : null
-                    }
-                    <AlertInput type="text" placeholder="여기에 입력하세요" value={userInput} onInput={(e) => {
+                    } */}
+                        {/* <AlertInput type="text" placeholder="여기에 입력하세요" value={userInput} onInput={(e) => {
                             isNaN(e.target.value) ? 
                             setIntAlert(true) : 
                             setUserInput(e.target.value)
                         }}>
-                    </AlertInput>
-                    <div className="col-md-6">
-                        <p>{item.content}</p>
-                        <p>{item.price}</p>
-                        <button className="btn btn-danger">주문하기</button>
-                    </div>
-                </>
+                    </AlertInput> */}
+                        <div className="col-md-6">
+                            <h4>{item.title}</h4>
+                            <p>{item.content}</p>
+                            <p>{item.price}</p>
+                            <button className="btn btn-danger">주문하기</button>
+                        </div>
+                    </>
                 }
-                
             </div>
-        </div> 
+            <Nav variant="tabs" defaultActiveKey="/home"> 
+                <Nav.Item onClick={ () => {setTab(0)}}>
+                    <Nav.Link eventKey="link0">버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item onClick={ () => {setTab(1)}}>
+                    <Nav.Link eventKey="link1">버튼2</Nav.Link>
+                </Nav.Item>
+                <Nav.Item onClick={ () => {setTab(2)}}>
+                    <Nav.Link eventKey="link3">버튼3</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <TabContent tab={tab} shoes={props.shoes}/>
+                
+
+            {/* <Tabs
+                defaultActiveKey="home"
+                transition={false}
+                className="mb-3"
+                justify
+            >   
+                <Tab eventKey="home" title="버튼 1">
+                    내용 1
+                </Tab>
+                <Tab eventKey="profile" title="버튼 2">
+                    <Row>
+                        <Col md={12}> 
+                            내용 2
+                        </Col>
+                    </Row>
+                </Tab>
+                <Tab eventKey="longer-tab" title="버튼 3">
+                    내용 3
+                </Tab>
+            </Tabs> */}
+        </div>
 
 
     )
 }
+
+
+function TabContent({tab}) {
+    let {storage} = useContext(Context1)
+    let value = [<div >내용1</div>, <div>내용2</div>, <div>내용3</div>]
+    let [fade, setFade] = useState(''); 
+
+    useEffect(()=> {
+        let a = setTimeout(()=> {setFade('end')}, 100)
+        return () => {
+            clearTimeout(a)
+            setFade('')
+        }
+    }, [tab])
+
+    return (
+        <div className= {"start " + fade}>
+            {value[tab]}
+        </div>
+    )
+    
+}
+
+
 
 
 export { Detail };
